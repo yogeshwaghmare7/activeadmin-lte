14ab8bfa8c069ecb04779c3a56fbd0277a51bff0
Armand Niampa <armand.niampa@gmail.com>
2015-11-28 23:30:14 +0100

    Update ActiveAdmin::LTE::Views::IndexAsTable for render actions.

--- lib/active_admin/LTE/views/index_as_table.rb
+++ lib/active_admin/LTE/views/index_as_table.rb
@@ -26,7 +26,7 @@ module ActiveAdmin
         def default_table
           proc do
             selectable_column
-            id_column
+            id_column if resource_class.primary_key # View based Models have no primary_key
             resource_class.content_columns.each do |col|
               column col.name.to_sym
             end
@@ -47,13 +47,20 @@ module ActiveAdmin
           # Display a column for checkbox
           def selectable_column
             return unless active_admin_config.batch_actions.any?
-            column resource_selection_toggle_cell, class: 'selectable' do |resource|
+            column resource_selection_toggle_cell, class: 'selectable', sortable: false do |resource|
               resource_selection_cell resource
+            end
+          end
+
+          def index_column(start_value = 1)
+            column '#', class: 'col-index', sortable: false do |resource|
+              @collection.offset_value + @collection.index(resource) + start_value
             end
           end
 
           # Display a column for the id
           def id_column
+            raise "#{resource_class.name} as no primary_key!" unless resource_class.primary_key
             column(resource_class.human_attribute_name(resource_class.primary_key), sortable: resource_class.primary_key) do |resource|
               if controller.action_methods.include?('show')
                 link_to resource.id, resource_path(resource), class: "resource_id_link"
@@ -61,6 +68,10 @@ module ActiveAdmin
                 resource.id
               end
             end
+          end
+
+          def default_actions
+            raise '`default_actions` is no longer provided in ActiveAdmin 1.x. Use `actions` instead.'
           end
 
           # Add links to perform actions.
@@ -93,11 +104,13 @@ module ActiveAdmin
           # end
           #
           # ```
+
           def actions(options = {}, &block)
             name          = options.delete(:name)     { '' }
             defaults      = options.delete(:defaults) { true }
             dropdown      = options.delete(:dropdown) { false }
             dropdown_name = options.delete(:dropdown_name) { I18n.t 'active_admin.dropdown_actions.button_label', default: 'Actions' }
+            options[:class] ||= 'col-actions'
 
             column name, options do |resource|
               if dropdown
@@ -106,7 +119,7 @@ module ActiveAdmin
                   instance_exec(resource, &block) if block_given?
                 end
               else
-                text_node default_actions(resource) if defaults
+                text_node defaults(resource) if defaults
                 text_node instance_exec(resource, &block) if block_given?
               end
             end
@@ -127,27 +140,36 @@ module ActiveAdmin
             end
           end
 
-          def default_actions(resource)
+          def defaults(resource, options = {})
             links = '<div class="text-center table-action"><div class="btn-group">'
+
             if controller.action_methods.include?('show') && authorized?(ActiveAdmin::Auth::READ, resource)
-              links << link_to(I18n.t('active_admin.view'), resource_path(resource), class: 'member_link view_link btn btn-xs btn-info')
+              links << link_to(I18n.t('active_admin.view'), resource_path(resource), class: "member_link view_link btn btn-xs btn-info #{options[:css_class]}")
             end
+
             if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
               links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), class:
-
-
-
-
-               'member_link edit_link btn btn-xs bg-orange')
+               "member_link edit_link btn btn-xs bg-orange #{options[:css_class]}")
             end
+
             if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), class: 'member_link delete_link btn btn-xs btn-danger',
+              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), class: "member_link delete_link btn btn-xs btn-danger #{options[:css_class]}",
                 method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')})
             end
+
             links << '</div></div>'
             links.html_safe
           end
+
+          class TableActions < ActiveAdmin::Component
+            builder_method :table_actions
+
+            def item *args
+              text_node link_to *args
+            end
+          end
         end # IndexTableFor
+
       end # IndexAsTable
     end
   end
