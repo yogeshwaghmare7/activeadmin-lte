91852c3193ce230a27f9525d64bba0c0eb1c46ae
Armand Niampa <armand.niampa@gmail.com>
2015-11-28 22:49:50 +0100

    Fix is_boolean method bug in lib/active_admin/LTE/views/components/table_for.rb.

--- lib/active_admin/LTE/views/index_as_table.rb
+++ lib/active_admin/LTE/views/index_as_table.rb
@@ -133,7 +133,12 @@ module ActiveAdmin
               links << link_to(I18n.t('active_admin.view'), resource_path(resource), class: 'member_link view_link btn btn-xs btn-info')
             end
             if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), class: 'member_link edit_link btn btn-xs bg-orange')
+              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), class:
+
+
+
+
+               'member_link edit_link btn btn-xs bg-orange')
             end
             if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
               links << link_to(I18n.t('active_admin.delete'), resource_path(resource), class: 'member_link delete_link btn btn-xs btn-danger',
